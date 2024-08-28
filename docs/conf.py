b0676a3f4c9631e0a5f9446d2a2105b104340593
Jessica Mitchell <j.mitchell@fz-juelich.de>
2024-08-28 10:51:35 +0200

    try filename_pattern

--- docs/conf.py
+++ docs/conf.py
@@ -132,7 +132,7 @@ sphinx_gallery_conf = {
     'examples_dirs': '../examples',   # path to your example scripts
     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
     'plot_gallery': 'False',
-    # 'filename_pattern': '/',
+    'filename_pattern': '/plot_',
     'ignore_pattern': r'.*util\.py'
 }
 
