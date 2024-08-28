bdeb95d5d8d498fd16669be797a5f6a3b40381d2
Jessica Mitchell <j.mitchell@fz-juelich.de>
2024-08-28 10:57:19 +0200

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ napoleon_use_keyword = True
 napoleon_custom_sections = None
 
 
-#nbsphinx_allow_errors = True
+# nbsphinx_allow_errors = True
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['']
 
@@ -131,7 +131,7 @@ add_module_names = False
 sphinx_gallery_conf = {
     'examples_dirs': '../examples',   # path to your example scripts
     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
-    'plot_gallery': 'False',
+    'plot_gallery': False,
     'filename_pattern': '/plot_',
     'ignore_pattern': r'.*util\.py'
 }
